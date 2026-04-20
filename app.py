718800121a09dd2ac98a73e6bf67eea476e82cf6
Ralph Ryan Espinile <ralckyespinile@example.com>
2026-04-20 15:57:24 +0800

    lazy load model

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, send_from_directory
-from ultralytics import YOLO
+from flask import Flask, render_template, request
 from PIL import Image
 import os
+import torch
 
 app = Flask(__name__)
-
-model = YOLO("helmet_best.pt")
-
 os.makedirs("static/uploads", exist_ok=True)
+
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        from ultralytics import YOLO
+        torch.set_num_threads(1)
+        model = YOLO("helmet_best.pt")
+        model.to("cpu")
+    return model
 
 @app.route("/")
 def home():
@@ -18,8 +26,10 @@ def predict():
     file = request.files["image"]
     filepath = os.path.join("static/uploads", "detected.jpg")
     img = Image.open(file).convert("RGB")
+    img = img.resize((320, 320))
     img.save(filepath)
-    results = model(filepath)
+    m = get_model()
+    results = m(filepath, imgsz=320)
     results[0].save(filename=filepath)
     return render_template("index.html", image="static/uploads/detected.jpg")
 
